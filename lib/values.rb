b3b3999f38c4ae593c16e20246bd388c9c5e5d1b
John Werenski <jtwerenski@gmail.com>
2018-09-16 18:32:37 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,10 +10,8 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
-  groceries.values 
-    binding.pry
-    groceries.values.flatten
-    groceries.values.reduce(:concat)
-  #end 
-  #binding.pry
+  #groceries.values 
+   # binding.pry
+  groceries.values.flatten
+
 end
